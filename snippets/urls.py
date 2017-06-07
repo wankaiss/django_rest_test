36fbc6319f5796909df1ca9741ee8ec7239bc6ec
yangqianhui <yangqianhui@youbanban.com>
2017-06-07 18:03:30 +0800

    完整练习完django_rest_framework的练习代码

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url, include
 from snippets import views
-from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+schema_view = get_schema_view(title='Pastebin API')
+
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-	url(r'^snippets/$', views.SnippetList.as_view()),
-	url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-	url(r'^users/$', views.UserList.as_view()),
-	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^schema/$', schema_view),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
-	url(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
-]
